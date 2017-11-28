ccc105523b3eac601030aca30e410ee7aef9d337
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2017-11-29 00:44:54 +0100

    Add more tests

--- test/support/sort_main_tests.ex
+++ test/support/sort_main_tests.ex
@@ -13,6 +13,12 @@ defmodule Sort.SortMainTests  do
       @second_list [1, 2, 3, 1, 2, 3, 4, 1, 2]
       @second_expected [1, 1, 1, 2, 2, 2, 3, 3, 4]
 
+      @no_change_list [2, 3, 4, 7, 7, 12,19]
+      @no_change_expected [2, 3, 4, 7, 7, 12,19]
+
+      @reverse_list [19, 12, 7, 7, 4, 3, 2]
+      @reverse_expected [2, 3, 4, 7, 7, 12,19]
+
       @moduletag unquote(options)
       
       test "truth" do
@@ -29,6 +35,14 @@ defmodule Sort.SortMainTests  do
 
       test "Other list should be sorted", %{sort_fun: sort_fun} do
 	assert @second_expected == sort_fun.(@second_list)
+      end
+
+      test "Sort an already sorted list", %{sort_fun: sort_fun} do
+	assert @no_change_expected == sort_fun.(@no_change_list)
+      end
+
+      test "Reverse a list", %{sort_fun: sort_fun} do
+	assert @reverse_expected == sort_fun.(@reverse_list)
       end
 
     end
